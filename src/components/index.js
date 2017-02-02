d6329097c0546e956c262f74118ac450cf1be26e
Colin Smith <cmsgoboston41@gmail.com>
2017-02-01 19:19:31 -0500

    Add nested routes

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,17 @@
+import MainMenu from './ui/MainMenu'
+
+export const Left = ({ children }) =>
+	<div className="page">
+		<MainMenu className="main-menu"/>
+		{children}
+	</div>
+
+export const Right = ({ children }) =>
+	<div className="page">
+		{children}
+		<MainMenu className="main-menu"/>
+	</div>
+
 export const Error404 = ({ location }) =>
     <div>
         <h1>Error404 resource not found</h1>
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Router, Route, hashHistory } from 'react-router'
 import Home from './components/ui/Home'
-import  { Error404  } from './components'
+import About from './components/ui/About'
+import MemberList from './components/ui/MemberList'
+import  { Left, Right, Error404  } from './components'
 
 const routes = (
     <Router history={hashHistory}>
         <Route path="/" component={Home} />
+        <Route path="/" component={Left}>
+        	<Route path="about" component={About} />
+        	<Route path="members" component={MemberList} />
+        </Route>
         <Route path="*" component={Error404} />
     </Router>
 )
