4a58c7d0cba12223376602d6d677798781337bdd
Colin Smith <cmsgoboston41@gmail.com>
2017-02-04 15:35:41 -0500

    Add member and member list

--- src/components/ui/MemberList.js
+++ src/components/ui/MemberList.js
@@ -1,11 +1,42 @@
 import { Component } from 'react'
 import fetch from 'isomorphic-fetch'
+import Member from './Member'
 
 class MemberList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            members: [
+            {
+                name: "Colin Smith",
+                email: "colin.smith@example.com",
+                thumbnail: "https://randomuser.me/api/portraits/men/53.jpg"
+            },
+            {
+                name: "Bill Belicheck",
+                email: "bill.belicheck@example.com",
+                thumbnail: "https://randomuser.me/api/portraits/men/74.jpg"
+            },
+            {
+                name: "J.K. Rowling",
+                email: "jk.rowling@example.com",
+                thumbnail: "https://randomuser.me/api/portraits/women/34.jpg"
+            }
+          ]
+        }
+    }
+
     render() {
+    	const { members } = this.state
         return (
             <div className="member-list">
             	<h1>Society Members</h1>
+				{members.map(
+					(data, i) => 
+						<Member key={i} 
+								onClick={email => console.log(email)}
+								{...data} />
+				)}
             </div>
         )    
    }     
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router, Route, hashHistory } from 'react-router'
 import Home from './components/ui/Home'
 import About from './components/ui/About'
 import MemberList from './components/ui/MemberList'
-import  { Left, Right, Error404  } from './components'
+import  { Left, Right, Whoops404  } from './components'
 
 const routes = (
     <Router history={hashHistory}>
@@ -12,7 +12,7 @@ const routes = (
         	<Route path="about" component={About} />
         	<Route path="members" component={MemberList} />
         </Route>
-        <Route path="*" component={Error404} />
+        <Route path="*" component={Whoops404} />
     </Router>
 )
 
